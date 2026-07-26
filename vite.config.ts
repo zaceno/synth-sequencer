201df3434d4745ec31f1bed5ddbebfe3a1e72ee2
Zacharias Enochsson <zacharias.enochsson@gmail.com>
2026-07-27 01:45:31 +0200

    setup publishing to ghpages

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite"
 import hyperapp from "vite-plugin-hyperapp"
 export default defineConfig({
+  base: "/synth-sequencer/",
   plugins: [hyperapp({ hmr: false })],
 })
